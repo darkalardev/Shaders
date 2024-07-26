2e34711a95dbeb6afdb3ecdf87c4eff42267406d
Agustin Adducci <recofskydev@gmail.com>
2024-07-26 20:27:20 -0300

    Snow shader

--- Shaders/Assets/_Game/Shaders/Dissolve/DissolveController.cs
+++ Shaders/Assets/_Game/Shaders/Dissolve/DissolveController.cs
@@ -1,17 +1,36 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class DissolveController : MonoBehaviour
 {
+    [Header("---- DISSOLVE ----")]
     [SerializeField] private float _dissolveSpeed = 1;
     [SerializeField] private float _dissolveWait = 1;
+    [Space]
+    [SerializeField] private SkinnedMeshRenderer _dissolveMesh;
+    [SerializeField] private int _dissolveMeshIndex;
 
+    [Header("---- REFERENCES ----")]
     [SerializeField] private Material _dissolveMaterial;
+    [SerializeField] private Material _dissolveMaterial_2;
 
     private bool _isDissolving;
     private float _dissolveValue;
     private float _valueStart = 1;
     private float _valueEnd = 0;
+
+    // for optimization
+    private int _hash_Dissolve = Shader.PropertyToID("_Dissolve");
+
+    private void Start()
+    {
+        _dissolveMaterial = _dissolveMesh.materials[0];
+        _dissolveMaterial_2 = _dissolveMesh.materials[1];
+        
+        _dissolveMaterial.SetFloat(_hash_Dissolve, _valueStart);
+        _dissolveMaterial_2.SetFloat(_hash_Dissolve, _valueStart);
+    }
 
     private void Update()
     {
@@ -27,17 +46,19 @@ public class DissolveController : MonoBehaviour
         while (_dissolveValue > _valueEnd)
         {
             _dissolveValue -= Time.deltaTime * _dissolveSpeed;
-            _dissolveMaterial.SetFloat("_Dissolve", _dissolveValue);
+            _dissolveMaterial.SetFloat(_hash_Dissolve, _dissolveValue);
+            _dissolveMaterial_2.SetFloat(_hash_Dissolve, _dissolveValue);
             yield return null;
         }
 
         _dissolveValue = _valueEnd;
         yield return new WaitForSeconds(_dissolveWait);
 
-        while (_dissolveValue < _valueEnd)
+        while (_dissolveValue < _valueStart)
         {
             _dissolveValue += Time.deltaTime * _dissolveSpeed;
-            _dissolveMaterial.SetFloat("_Dissolve", _dissolveValue);
+            _dissolveMaterial.SetFloat(_hash_Dissolve, _dissolveValue);
+            _dissolveMaterial_2.SetFloat(_hash_Dissolve, _dissolveValue);
             yield return null;
         }
         _isDissolving = false;
